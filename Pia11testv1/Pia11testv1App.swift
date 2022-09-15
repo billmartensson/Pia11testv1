f7b99df582473d02020fba2bfc24012944858c12
Bill Mårtensson <bill@magictechnology.se>
2022-09-15 10:39:02 +0200

    Initial Commit

---
+++ Pia11testv1/Pia11testv1App.swift
@@ -0,0 +1,17 @@
+//
+//  Pia11testv1App.swift
+//  Pia11testv1
+//
+//  Created by Bill Martensson on 2022-09-15.
+//
+
+import SwiftUI
+
+@main
+struct Pia11testv1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
